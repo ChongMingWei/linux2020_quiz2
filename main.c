97de2f20aba62a08dafdb4544c08eb998bb4f887
ChongMingWei <29A7882zz@gmail.com>
2020-09-26 23:57:19 +0800

    Modify Fast Division in test 3

--- main.c
+++ main.c
@@ -1,13 +1,43 @@
-#include "quiz2_5.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "quiz2_3.h"
+#include <time.h>
 int main(){
-        /* quote from https://www.gutenberg.org/files/74/74-h/74-h.htm */
-    char *str =
-        "This eBook is for the use of anyone anywhere at no cost and with \
-almost no restrictions whatsoever.  You may copy it, give it away or \
-re-use it under the terms of the Project Gutenberg License included \
-with this eBook or online at www.gutenberg.net";
-    int n = strlen(str);
-    strlower_vector(str, n);
-    printf("%s",str);
+    // Timer 
+    struct timespec time1 = {0, 0};
+    struct timespec time2 = {0, 0};
+    // Division 
+    uint32_t q,d,n;
+    srand(time(NULL));
+    for (d = 2;d < 5000;++d){
+
+        long fast=0;
+        long normal=0;
+        for (int i=0; i<5;++i){
+            n = rand() % 4294967296;
+            
+            clock_gettime(CLOCK_REALTIME, &time1);
+            q = n / d;
+            clock_gettime(CLOCK_REALTIME, &time2);
+            
+            long diff = time2.tv_nsec - time1.tv_nsec;
+            normal += diff;
+ 
+            // fast division part
+            uint64_t two_to_k = ((uint64_t)1 << 32);
+            uint32_t magic = (uint32_t)(two_to_k / d);
+            if (two_to_k % d != 0) {
+	            magic++;
+            }
+            
+            clock_gettime(CLOCK_REALTIME, &time1);
+            q = ((uint64_t)n * (uint64_t)magic) >> 32;
+            clock_gettime(CLOCK_REALTIME, &time2);
+
+            diff = time2.tv_nsec - time1.tv_nsec;
+            fast += diff;
+        }
+        printf("fast = %ld ns normal = %ld ns\n",fast/5,normal/5);
+    }
     return 0;
 }
--- quiz2_3.h
+++ quiz2_3.h
@@ -5,7 +5,13 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+
 uint32_t quickmod(uint32_t n);
-size_t div_compute(size_t n, size_t d);
 bool divisible(uint32_t n);
+typedef struct div_info_s div_info_t;
+struct div_info_s {
+    uint32_t magic;
+};
+void div_init(div_info_t *div_info, size_t d);
+size_t div_compute(div_info_t *div_info, size_t n);
 #endif
