e23613f230c0e42918dfbcfa49120db38b7a1253
ChongMingWei <29A7882zz@gmail.com>
2020-09-26 15:10:25 +0800

    Complete Quiz2 Test6

---
+++ quiz2_6.h
@@ -0,0 +1,9 @@
+#ifndef QUIZ2_6_H
+#define QUIZ2_6_H
+/* Single Number II problem from leetcode
+ * https://leetcode.com/problems/single-number-ii/
+ */
+int singleNumber_repeat3(int *nums, int numsSize);
+int singleNumber_repeat4(int *nums, int numsSize);
+int singleNumber_repeat5(int *nums, int numsSize);
+#endif
